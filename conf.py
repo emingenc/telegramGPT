b45fc3358965daf9c72138d7abc1cba1a2e2e513
emingenc <memn.gnc@gmail.com>
2024-10-03 23:10:22 -0700

    add ollama support

--- conf.py
+++ conf.py
@@ -1,6 +1,11 @@
 from langchain.embeddings.base import Embeddings
 from langchain.llms.base import BaseLLM
 from langchain.tools.base import BaseTool
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_ollama import ChatOllama
+from langchain_ollama import OllamaEmbeddings
+
 import os 
 
 USERS = os.getenv("USERS", "")
@@ -8,15 +13,26 @@ USERS = os.getenv("USERS", "")
 users = [user.strip() for user in USERS.split(",")]
 
 
-def get_llm() -> BaseLLM:
+def get_llm(llmname) -> BaseLLM:
     """Function to get the LLM. Replace with your desired LLM."""
-    from langchain_openai import ChatOpenAI
-    return ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+    if llmname == "gpt-4o-mini":
+        return ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+    if llmname == "llama3.2":
+        return ChatOllama(
+            model="llama3.2",
+            temperature=0,
+            # other params...
+        )
 
-def get_embeddings() -> Embeddings:
+def get_embeddings(embeddingmodel) -> Embeddings:
     """Function to get the embeddings model. Replace with your desired embeddings."""
-    from langchain_openai import OpenAIEmbeddings
-    return OpenAIEmbeddings()
+    if embeddingmodel == "openai":
+        return OpenAIEmbeddings()
+    if embeddingmodel == "ollama":
+        embed = OllamaEmbeddings(
+            model="llama3"
+        )
+        return embed
 
 def get_web_search_tool() -> BaseTool:
     """Function to get the web search tool. Replace with your implementation."""
@@ -24,6 +40,8 @@ def get_web_search_tool() -> BaseTool:
     return TavilySearchResults(k=3)
 
 # Initialize components
-llm = get_llm()
-embeddings = get_embeddings()
+# llm = get_llm('llama3.2')
+llm = get_llm('gpt-4o-mini')
+# embeddings = get_embeddings('ollama')
+embeddings = get_embeddings('openai')
 web_search_tool = get_web_search_tool()
